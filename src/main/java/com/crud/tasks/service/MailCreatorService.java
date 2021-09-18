f5d1fba46d9f0a570366ddc1e6380178cf23b191
jagda <danielzychniewicz@wp.pl>
2021-09-18 18:12:30 +0200

    32.2 Task - set other values

--- src/main/java/com/crud/tasks/service/MailCreatorService.java
+++ src/main/java/com/crud/tasks/service/MailCreatorService.java
@@ -19,10 +19,17 @@ public class MailCreatorService {
 
     public String buildTrelloCardEmail(String message) {
         Context context = new Context();
+        context.setVariable("preview_message", "The message was generated automatically - do not reply.");
         context.setVariable("message", message);
         context.setVariable("tasks_url", "http://127.0.0.1:5500/");
         context.setVariable("button", "Visit website");
         context.setVariable("admin_name", adminConfig.getAdminName());
+        context.setVariable("regards_part1", "Best regards, ");
+        context.setVariable("regards_part2", "CRUD support team.");
+        context.setVariable("company_name", adminConfig.getCompanyName());
+        context.setVariable("company_goal", adminConfig.getCompanyGoal());
+        context.setVariable("company_email", adminConfig.getCompanyEmail());
+        context.setVariable("company_phone", adminConfig.getCompanyPhone());
         return templateEngine.process("mail/created-trello-card-mail", context);
     }
 }
